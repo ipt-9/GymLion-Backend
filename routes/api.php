5dbe3b080ef03a99cdd91eacc07a44da4d4d1b4d
Gianmarco Wyss <Gianmarco.Wyss@icloud.com>
2024-03-28 17:13:54 +0100

    users api fixes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/users', 'UserController@index');
+Route::get('users', [UserController::class, 'index']);
 
